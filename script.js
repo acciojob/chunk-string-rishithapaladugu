3148763ee31c4a3eee34dd42660ebe7f0fcf0bd1
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-01 12:10:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function stringChop(str, size) {
   // your code here
 	const result = [];
 	for(let i = 0; i < str.length; i += size){
-		result.push(str.slice(i, i + size));
+		result.push(str.substring(i, i + size));
 	}
 	return result; 
 	
 	
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
